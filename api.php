1f5edf805bc15294efa7f6752b0da3f431b80b67
Priyank Upadhyay <priyank@platinos.in>
2017-09-29 01:19:42 +0530

    test

--- api.php
+++ api.php
@@ -215,28 +215,8 @@ if($data = mysqli_fetch_assoc($result)){
 echo json_encode($response);
 
 }
-
-/***********************************************************/
-/*********************  *********************/
-/***********************************************************/
-
 function addsubject(){
-
-include 'dbconnect.php';
-
-$subject_name = $_POST['subject_name']
 $response = array("error" => FALSE);
-$sql = "INSERT INTO subjects (subject)
-		VALUES ('$subject_name')";
-
-if (mysqli_query($conn, $sql)) {
-	$response["error"] = FALSE;
-	$response["msg"] = "Subject added successfully!";
-} else {
-    $response["error"] = TRUE;
-    $response["error_msg"] = "Subject could not be added!";
-}
-
 echo json_encode($response);
 
 }
