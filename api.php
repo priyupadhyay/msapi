348ba165f37e12f8f26643813aadffc16ca9939a
Priyank Upadhyay <priyank@platinos.in>
2017-10-29 14:31:58 +0530

    nn

--- api.php
+++ api.php
@@ -347,39 +347,6 @@ echo json_encode($response);
 
 
 
-/***********************************************************/
-/********************* Get Chapters By Id *********************/
-/***********************************************************/
-
-function getchaptersbyid($ch_id){
-$response = array("error" => FALSE);
-if($ch_id != "" ){
-return "error";
-}
-else{
-include 'dbconnect.php';
-
-
-$sql = "SELECT * FROM chapters where id = $ch_id";
-if ($result = mysqli_query($conn, $sql)) {
-	
-
-$data = mysqli_fetch_assoc($result)
-	return $data['name'];
-
-
-
-
-} else {
-    return "error";
-}
-
-}
-
-
-}
-
-
 
 /***********************************************************/
 /********************* Get Chapters *********************/
