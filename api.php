41e95bda4dba99be0564fc61b2d95de630edba00
Priyank Upadhyay <priyank@platinos.in>
2017-10-29 14:55:36 +0530

    hvh

--- api.php
+++ api.php
@@ -565,7 +565,26 @@ echo json_encode($response);
 /***********************************************************/
 
 
+function getchaptersbyid($ch_id){
 
+if($ch_id == ""){
+return "error";
+}
+else{
+include 'dbconnect.php';
+$sql = "SELECT * FROM chapters where id = $ch_id";
+if ($result = mysqli_query($conn, $sql)) {
+	$data = mysqli_fetch_assoc($result);
+	return $data['chap'];
+
+} 
+else {
+    return "error";
+}
+}
+
+
+}
 
 
 
