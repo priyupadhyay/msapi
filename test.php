7ace5309122050440a8fe067884c43a3c01761e0
Priyank Upadhyay <priyank@platinos.in>
2017-10-29 14:42:52 +0530

    sdfsd

---
+++ test.php
@@ -0,0 +1,26 @@
+<?
+
+function getchaptersbyid($ch_id){
+$response = array("error" => FALSE);
+if($ch_id != ""){
+return "error";
+}
+else{
+include 'dbconnect.php';
+$sql = "SELECT * FROM chapters where id = $ch_id";
+if ($result = mysqli_query($conn, $sql)) {
+	$data = mysqli_fetch_assoc($result)
+	return $data['chap'];
+
+} 
+else {
+    return "error";
+}}
+
+
+}
+
+if(getchaptersbyid("1")){
+	echo "hello";
+}
+?>
