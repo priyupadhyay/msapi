41999959155b98a52b7bfd00c39b1680baf23ae1
Priyank Upadhyay <priyank@platinos.in>
2018-01-28 19:43:05 +0530

    qwerty

--- api.php
+++ api.php
@@ -13,6 +13,10 @@ if(isset($_POST["func"]) && !empty($_POST["func"])){
 
 		viewquestion();
 		break;
+		case 'viewquestionsbysubject':
+
+		viewquestionsbysubject();
+		break;
 
 		case 'viewsubject':
 
@@ -161,6 +165,46 @@ function viewquestion(){
 
 }
 
+
+/***********************************************************/
+/****************** View Questions By Subject ****************/
+/***********************************************************/
+
+function viewquestionsbysubject(){
+
+	include 'dbconnect.php';
+
+	$sub = $_POST['subject'];
+	$response = array("error" => FALSE);
+
+	$sql = "SELECT * FROM questions WHERE `status`= 1 AND `subject` = '$sub'";
+	$result = mysqli_query($conn, $sql);
+	$i=0;
+
+	while($data = mysqli_fetch_assoc($result)){
+		$response["error"] = FALSE;
+		$response["data"][$i]["id"] = $data["id"];
+		$response["data"][$i]["class"] = $data["class"];
+		$response["data"][$i]["type"] = $data["type"];
+		$response["data"][$i]["subject"] = $data["subject"];
+		$response["data"][$i]["chapter"] = $data["chapter"];
+		$response["data"][$i]["level"] = $data["level"];
+		$response["data"][$i]["tag"] = $data["tag"];
+		$response["data"][$i]["topic"] = $data["topic"];
+		$response["data"][$i]["marks"] = $data["marks"];
+		$response["data"][$i]["ques_txt"] = $data["ques_txt"];
+		$response["data"][$i]["ques_img"] = $data["ques_img"];
+		$response["data"][$i]["qr"] = "plati017".sprintf('%07d', $data['id']);
+		$response["data"][$i]["answer"] = $data["answer"];
+		$response["data"][$i]["youtube"] = $data["youtube"];
+		$i++;
+
+	}
+	$response["data"]["size"] = $i;
+
+	echo json_encode($response);
+
+}
 
 
 
